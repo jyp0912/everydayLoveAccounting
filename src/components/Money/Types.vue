4a247612e192b3d24859c6d93685702fadbcafeb
oliver <896986794@qq.com>
2020-04-04 20:52:35 +0800

    用TS重写Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,29 +1,41 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type === '-' && 'selected'">支出</li>
-      <li :class="type === '+' && 'selected'">收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script>
-    export default {
-        name: 'Types',
-        data(){
-            return {
-                type: '-' //'-'表示支出，'+'表示收入
-            }
-        },
-        methods:{
-            selectType(type){
-                if (type!== '-' && type !== '+'){
-                    throw new Error('type is unknown')
-                }
-                this.type = type
-            }
-        }
-    };
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from "vue-property-decorator";
+  @Component
+  export default class Type extends Vue{
+      type = '-';
+      selectType(type: string){
+          if (type!== '-' && type !== '+'){
+              throw new Error('type is unknown')
+          }
+          this.type = type
+      }
+  }
+    // export default {
+    //     name: 'Types',
+    //     data(){
+    //         return {
+    //             type: '-' //'-'表示支出，'+'表示收入
+    //         }
+    //     },
+    //     methods:{
+    //         selectType(type){
+    //             if (type!== '-' && type !== '+'){
+    //                 throw new Error('type is unknown')
+    //             }
+    //             this.type = type
+    //         }
+    //     }
+    // };
 </script>
 
 <style lang="scss" scoped>
