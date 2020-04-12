eece54ce54bcecda0ffaa219f30900f1c5e97890
oliver <896986794@qq.com>
2020-04-12 15:00:17 +0800

    当用户用非手机打开网页时提示用户扫描二维码

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,26 @@ Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
 
 
-
 new Vue({
     router,
     store,
     render: h => h(App)
 }).$mount('#app');
 
-window.onload=function(){
+window.onload = function () {
     setTimeout(function () {
-    window.scrollTo(0,9999);
-    },0)
+        window.scrollTo(0, 9999);
+    }, 0);
 };
+
+if (document.documentElement.clientWidth >= 500) {
+    window.alert('请使用手机打开网页以保证浏览效果，点击确定弹出二维码');
+    const img = document.createElement('img');
+    img.src = '/qrcode.png';
+    img.style.position = 'fixed';
+    img.style.left = '50%';
+    img.style.top = '50%';
+    img.style.transform = 'translate(-50%,-50%)';
+    img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+    document.body.appendChild(img);
+}
